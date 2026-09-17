7affb73fb830b815d7f0887c6cbf19b0ebf9443c
iamkishorkp18 <kishorkishukp18@gmail.com>
2026-09-17 15:45:29 +0530

    Update apiClient.js

--- src/Api/apiClient.js
+++ src/Api/apiClient.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://localhost:8080',
+  baseURL: 'https://water-billing-system-backend.onrender.com',
   headers: { 'Content-Type': 'application/json' }
 });
 
